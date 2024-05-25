eb0a417966704c8fd6c8ac376b19b4021ebb04ad
包子 <chengbaozi6@qq.com>
2024-05-25 23:03:46 +0800

    establish

---
+++ Assets/Scripts_Runtime/Main.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour {
+    void Awake() {
+        Debug.Log("Main.Awake");
+    }
+
+    void Update() {
+
+    }
+}
